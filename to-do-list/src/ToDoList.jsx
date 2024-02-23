3368025d199ed96175e7bfe785493910bc0723f9
Akkel-i <118986817+Akkel-i@users.noreply.github.com>
2024-02-23 15:30:05 +0200

    date and desc done

---
+++ to-do-list/src/ToDoList.jsx
@@ -0,0 +1,55 @@
+// import useState form react
+import { useState } from "react";
+
+function TodoList() {
+
+    // alustetaan statet
+    const [description, setDescription] = useState("");
+    const [todos, setTodos] = useState([]);
+    const [date, setDate] = useState("");
+
+    const handleChangeDescription = (event) => {
+        setDescription(event.target.value);
+    };
+    const handleChangeDate = (event) => {
+        setDate(event.target.value);
+    };
+
+    const addTodo = () => {
+        setTodos([...todos, { description: description, date: date }]);
+        setDescription("");
+        setDate("");
+    };
+
+    return (
+        <>
+        <div id="header">
+        <h1>ToDo List!!</h1>
+        </div>
+
+        <div id="body">
+            <input placeholder="Description" onChange={handleChangeDescription} value={description} />
+            <input placeholder="Date" onChange={handleChangeDate} value={date} />
+            <button onClick={addTodo}>Add</button>
+            <table>
+                <tbody>
+                <tr>
+                    <th>Description</th>
+                    <th>Date</th>
+                </tr>
+                </tbody>
+                <tbody>
+                    {todos.map((todo, index) => (
+                        <tr key={index}>
+                            <td>{todo.description}</td>
+                            <td>{todo.date}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            </div>
+        </>
+    );
+}
+
+export default TodoList;
